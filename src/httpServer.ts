4a50d699488ce7010a8b742c2656611b59b897a5
Justin Murray <justin@timescale.com>
2025-07-23 17:04:18 -0400

    DRY context

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
-import { Pool } from 'pg';
-
-import { ServerContext } from './types.js';
 import { httpServerFactory } from './shared/boilerplate/src/httpServer.js';
 import { apiFactories } from './apis/index.js';
-import { serverInfo } from './serverInfo.js';
-
-const pgPool = new Pool();
-const context: ServerContext = { pgPool };
+import { context, serverInfo } from './serverInfo.js';
 
 httpServerFactory({
   ...serverInfo,
--- src/stdio.ts
+++ src/stdio.ts
@@ -1,13 +1,7 @@
 #!/usr/bin/env node
-import { ServerContext } from './types.js';
-import { Pool } from 'pg';
-
 import { stdioServerFactory } from './shared/boilerplate/src/stdio.js';
 import { apiFactories } from './apis/index.js';
-import { serverInfo } from './serverInfo.js';
-
-const pgPool = new Pool();
-const context: ServerContext = { pgPool };
+import { context, serverInfo } from './serverInfo.js';
 
 stdioServerFactory({
   ...serverInfo,
